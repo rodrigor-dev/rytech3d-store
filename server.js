eff36cdba97bd606c0f9bbd76d9f93048e2db3d7
Rodrigo RYTECH3D <rodrigo@rytech3d.com.br>
2026-06-07 10:03:42 -0300

    Fix CSP blocking all inline event handlers via script-src-attr
    - Add scriptSrcAttr: ['unsafe-inline'] to allow onclick/oninput/onerror
    - helmet defaults script-src-attr to 'none' which blocks ALL inline handlers

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://cdnjs.cloudflare.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com", "https://cdnjs.cloudflare.com"],
       imgSrc: ["'self'", "data:", "blob:"],
